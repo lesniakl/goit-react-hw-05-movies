edef1ad04763b574f43265accdf564981d7c7961
Łukasz Leśniak <lukaszlesniaksw@gmail.com>
2023-12-07 20:59:58 +0100

    Add location to back button in MovieDetails

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,11 +1,21 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useTmdb } from 'hooks/useTmdb';
 import React, { useEffect, useState } from 'react';
-import { NavLink, Outlet, useParams } from 'react-router-dom';
+import {
+  Link,
+  NavLink,
+  Outlet,
+  useLocation,
+  useParams,
+} from 'react-router-dom';
 
 export default function MovieDetails() {
   const [movie, setMovie] = useState({});
   const { getDetails } = useTmdb();
   const { movieId } = useParams();
+  const location = useLocation();
+
+  const backLinkHref = location.state?.from ?? '/';
 
   const fillDetails = async () => {
     const details = await getDetails(movieId);
@@ -17,17 +27,29 @@ export default function MovieDetails() {
   }, []);
 
   return (
-    <div>
-      <img src={movie.poster} alt={movie.title} />
-      <h2>{movie.title}</h2>
-      <span>User score: {movie.score}</span>
-      <h3>Overview</h3>
-      <span>{movie.overview}</span>
-      <h4>Genres</h4>
-      <span>{movie.genres}</span>
-      <NavLink to="cast">Cast</NavLink>
-      <NavLink to="reviews">Reviews</NavLink>
+    <>
+      {movie.title && (
+        <div>
+          <Link to={backLinkHref}>Go back</Link>
+          <img src={movie.poster} alt={movie.title} />
+          <h2>
+            {movie.title} ({movie.release_year})
+          </h2>
+          <span>User score: {movie.score}</span>
+          <h3>Overview</h3>
+          <span>{movie.overview}</span>
+          <h4>Genres</h4>
+          <span>{movie.genres}</span>
+          <NavLink to="cast" state={{ from: backLinkHref }}>
+            Cast
+          </NavLink>
+          <NavLink to="reviews" state={{ from: backLinkHref }}>
+            Reviews
+          </NavLink>
+        </div>
+      )}
+
       <Outlet />
-    </div>
+    </>
   );
 }
