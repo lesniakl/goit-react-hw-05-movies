e06d39787cd0060c7843291da2d88d795e86e8eb
Łukasz Leśniak <lukaszlesniaksw@gmail.com>
2023-12-07 01:28:37 +0100

    Add Home component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,32 @@
-import React from 'react';
+import { useTmdb } from 'hooks/useTmdb';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
-  return <div>Home</div>;
+  const [movies, setMovies] = useState([]);
+  const { getTrending } = useTmdb();
+
+  const fillHome = async () => {
+    const trending = await getTrending();
+    setMovies(trending);
+  };
+
+  useEffect(() => {
+    fillHome();
+  }, []);
+
+  return (
+    <div>
+      <h1>Trending today</h1>
+      {movies &&
+        movies.map(movie => {
+          const movieLink = `/movies/${movie.id}`;
+          return (
+            <Link key={movie.id} to={movieLink}>
+              {movie.title}
+            </Link>
+          );
+        })}
+    </div>
+  );
 }
