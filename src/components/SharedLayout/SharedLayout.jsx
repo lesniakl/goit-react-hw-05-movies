a9515ed6958c6de34233d0e68f7b66050268e4f0
Łukasz Leśniak <lukaszlesniaksw@gmail.com>
2023-12-07 01:29:00 +0100

    Add SharedLayout component

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import { NavLink, Outlet } from 'react-router-dom';
 
 export default function SharedLayout() {
-  return <div>SharedLayout</div>;
+  return (
+    <div>
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="movies">Movies</NavLink>
+      </nav>
+      <Outlet />
+    </div>
+  );
 }
